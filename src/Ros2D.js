c3585fe8319e0903d52237126ef1782a4da4574c
Semih YOLCU <39175076+smhylc@users.noreply.github.com>
2022-06-08 10:09:18 +0300

    Update Ros2D.js

--- src/Ros2D.js
+++ src/Ros2D.js
@@ -33,5 +33,5 @@ createjs.Stage.prototype.rosQuaternionToGlobalTheta = function(orientation) {
   var q2 = orientation.y;
   var q3 = orientation.z;
   // Canvas rotation is clock wise and in degrees
-  return -Math.atan2(2 * (q0 * q3 + q1 * q2), 1 - 2 * (q2 * q2 + q3 * q3)) * 180.0 / Math.PI;
+  return -Math.atan2(2 * (q0 * q3 + q1 * q2), 1 - 2 * (q2 * q2 + q3 * q3)) * 90.0 / Math.PI;
 };
